bcf87ce9ec410f04a15ec75e406dbf0ae30b6521
Yao Xu <yaoxu369@gmail.com>
2025-06-24 13:55:26 -0400

    lab 5 init

---
+++ Lab 5/maze.py
@@ -0,0 +1,23 @@
+"""
+File: maze.py
+"""
+
+def main():
+    maze = getMazeFromFile()
+    print(maze)
+    (startRow, startCol) = findStartPos(maze)
+    success = getOut(startRow, startCol, maze)
+    if success:
+        print("Maze solved:")
+        print(maze)
+    else:
+        print("No path out of this maze")
+    
+def getMazeFromFile():
+
+def findStartPos(maze):
+                
+def getOut(row, column, maze):
+
+if __name__ == "__main__": 
+    main()
