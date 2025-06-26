e608eb5d1f7af93f4086676d1e3e5396b0f5e536
Yao Xu <yaoxu369@gmail.com>
2025-06-26 06:59:30 -0400

    lab 5 update_getMazeFromFile

--- Lab 5/maze.py
+++ Lab 5/maze.py
@@ -8,19 +8,23 @@ In this program, we implemented three methods: getMazeFromFile(), findStartPos()
 def main():
     maze = getMazeFromFile()
     print(maze)
-    (startRow, startCol) = findStartPos(maze)
-    success = getOut(startRow, startCol, maze)
-    if success:
-        print("Maze solved:")
-        print(maze)
-    else:
-        print("No path out of this maze")
+    # (startRow, startCol) = findStartPos(maze)
+    # success = getOut(startRow, startCol, maze)
+    # if success:
+    #     print("Maze solved:")
+    #     print(maze)
+    # else:
+    #     print("No path out of this maze")
     
 def getMazeFromFile():
+    with open("maze.txt") as f:
+        maze = f.readlines().rstrip('\n')
+        print(type(maze))
+        return maze
 
-def findStartPos(maze):
-                
-def getOut(row, column, maze):
+# def findStartPos(maze):
+#
+# def getOut(row, column, maze):
 
 if __name__ == "__main__": 
     main()
