192500bd4516c7480a7d0956764487414c50b711
Yao Xu <yaoxu369@gmail.com>
2025-06-26 07:22:22 -0400

    lab 5 update_getMazeFromFile

--- Lab 5/maze.py
+++ Lab 5/maze.py
@@ -4,11 +4,13 @@ Yao Xu
 
 In this program, we implemented three methods: getMazeFromFile(), findStartPos() and getOut().
 """
+from grid import Grid
+
 
 def main():
     maze = getMazeFromFile()
     print(maze)
-    # (startRow, startCol) = findStartPos(maze)
+    (startRow, startCol) = findStartPos(maze)
     # success = getOut(startRow, startCol, maze)
     # if success:
     #     print("Maze solved:")
@@ -18,12 +20,24 @@ def main():
     
 def getMazeFromFile():
     with open("maze.txt") as f:
-        maze = f.readlines().rstrip('\n')
-        print(type(maze))
-        return maze
+        maze = [line.rstrip('\n') for line in f]
+        grid_width = len(maze[0])
+        grid_height = len(maze)
+        grid = Grid(grid_height, grid_width)
 
-# def findStartPos(maze):
-#
+        for row in range(grid_height):
+            for col in range(grid_width):
+                grid[row][col] = maze[row][col]
+        return grid
+
+def findStartPos(maze):
+    for row in range (maze.getHeight()):
+        for col in range(maze.getWidth()):
+            if maze[row][col] == 'P':
+                print(row)
+                print(col)
+                return (row, col)
+
 # def getOut(row, column, maze):
 
 if __name__ == "__main__": 
