aca619e1d5d7bdd60a3085ae07b3a58e09cbad9e
Yao Xu <yaoxu369@gmail.com>
2025-07-01 08:58:28 -0400

    Exercise 17

---
+++ YaoXu_MidtermExam_157.py
@@ -0,0 +1,38 @@
+########################################
+#
+# CSC 157-8C1 Python Computer Science I
+# Dept. of CS - OAKTON COLLEGE
+# 
+# Summer 2025 - MIDTERM
+# Chapter 5: Programming  Exercise 17. Prime Number List
+# 
+# Date: [Put the date here]   <=========
+#
+# Author: [Put your name here] <======== IMPORTANT
+#
+########################################
+
+# Must use the following starter code
+# main function
+def main():
+    # local variable
+    totalNumbers = 100
+    print('number', '\t', 'is prime')
+    print('------------------------')
+
+    # For each number,
+    # IF number is prime: print (number, '\t', 'prime')
+    # ELSE: print (number, '\t', 'not prime')
+
+    WRITE the rest of YOUR CODE for the main() function HERE  
+
+# define the is_prime function 
+# it receives a number as a parameter,
+# and returns True if number is prime, False otherwise. 
+def is_prime(number):
+    
+    WRITE YOUR CODE for the is_prime(number) function HERE 
+
+# Call the main function.
+main()
+
