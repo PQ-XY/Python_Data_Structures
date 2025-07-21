908f9ed251abf81c1aa9f87e5f53d70dd56c247c
Yao Xu <yaoxu369@gmail.com>
2025-07-21 13:34:33 -0400

    Final Exam update

--- Python Computer Science I/YaoXu_FinalExam_157/YaoXu_FinalExam_157.py
+++ Python Computer Science I/YaoXu_FinalExam_157/YaoXu_FinalExam_157.py
@@ -17,27 +17,48 @@
 # min - for the minimum of all the ints
 # max - for the maximum of all the ints
 firstTime = True
-# min & max must be initalized in the loop
+# min & max must be initialized in the loop
 
-"""initiate variables"""
+#Initialize total, count, average variables
 total = 0
+count = 0
 average = 0.0
-min = 0
-max = 0
 
 fileName = "numbers.txt"
 
 # Open the numbers.txt file
-inFile = open(numbers.txt, mode="r")
+inFile = open('numbers.txt', mode="r")
 
 # In the following for-loop read the ints in the numbers.txt file and
 # compute  their total, average, min, and max 
 for line in inFile:
     #
     # Find min, max, average, total in this loop
-    # Remember: min & max must be initialized in the oop
+    # Remember: min & max must be initialized in the loop
     #
+    line_nums = list(map(int, line.split()))
 
+    #Initialize min and max
+    if firstTime and line_nums:
+        min = line_nums[0]
+        max = line_nums[0]
+        firstTime = False
+
+    for i in line_nums:
+        total += i
+        count += 1
+
+        #update min and max
+        if i < min:
+            min = i
+        if i > max:
+            max = i
+
+    #Calculate average with valid count value
+    if count > 0:
+        average = total / count
+    else:
+        average = 0
 
 inFile.close()
 
